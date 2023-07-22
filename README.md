6df99ddc30ddcf431425e1453f0c0fce7f68f97b
killianli <killianli@tencent.com>
2023-07-22 15:40:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trained Model on CSDS Dialogue Dataset
+
+This project is a fork of BERT part of https://github.com/xiaolinAndy/CSDS which is modifed to be suitbale for higher version of transformer and add a generate method for user dialogue summarization.
+
+The pretrained checkpoint is trained in user/abstraction model of original project.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="bert_summarizer",
+    version="0.0.1",
+    description="A bert-based natrual language neural network for chinese dialogue generation trained on CSDS dataset",
+    author="uiui",
+    author_email="mocoui@outlook.com",
+    install_requires=[
+        "torch>=1.6",
+        "transformers>=4.0.0",
+        "jieba",
+        "tqdm"
+    ],
+    # packages=find_packages(where="bert_summarizer"),
+    # package_dir={"": "bert_summarizer"},
+    include_package_data=True
+)
